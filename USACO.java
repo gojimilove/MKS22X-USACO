7b53144399835fce45fa3c88cca1a1abdbd3ad84
Jessica Kim <jkim00@stuy.edu>
2019-03-06 11:37:30 -0500

    can isolate the first 4 numbers and create an array with the starting levels

--- USACO.java
+++ USACO.java
@@ -2,5 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class USACO {
+  public static int bronze(String filename) throws FileNotFoundException{
+    int R = 0;
+    int C = 0;
+    int E = 0;
+    int N = 0;
+    File f = new File(filename);
+    Scanner s = new Scanner(f);
+    //set the first 4 numbers
+    if (s.hasNext()) R = Integer.parseInt(s.next());
+    if (s.hasNext()) C = Integer.parseInt(s.next());
+    if (s.hasNext()) E = Integer.parseInt(s.next());
+    if (s.hasNext()) N = Integer.parseInt(s.next());
+    //fill in the array
+    int[][] squares = new int[R][C];
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (s.hasNext()) squares[i][j] = Integer.parseInt(s.next());
+      }
+    }
+    //print array
+    for (int i = 0; i < squares.length; i++) {
+      for (int j = 0; j < squares[i].length; j++) {
+        System.out.print(squares[i][j] + " ");
+      }
+      System.out.print("\n");
+    }
+    return -1;
+  }
 
+  public static int silver(String filename) {
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    try {
+      bronze("makelake.1.in");
+    }catch(FileNotFoundException e) {
+      System.out.println("invalid filename");
+    }
+  }
 }
